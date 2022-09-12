286f49a331cf6a4b81339946e11bdd6c7a8d0ff4
Shani <shanaynayrom@gmail.com>
2022-09-12 16:40:54 +0300

    labels

--- chartJS/csv-chartjs/src/features/chartData.ts
+++ chartJS/csv-chartjs/src/features/chartData.ts
@@ -3,7 +3,7 @@
  const createChartData = (someData:any,backgroundcolor:any,Label:any,dataSetData:any) => {
   
     const data = {
-        labels: someData.map((data:any) => `${data[Label]}`),
+        labels: someData.map((data:any) => `${data.date.seconds}`),
         datasets: [
           {
             label: "Global Temperature Time Series,Annual , 1800 - present",
