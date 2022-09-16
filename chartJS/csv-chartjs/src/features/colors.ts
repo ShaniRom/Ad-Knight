fd3c74e462edafb5fc68691d61542e3cf8c9d7f0
Ido gino <davegino220@gmail.com>
2022-09-16 15:26:25 +0300

    test

--- chartJS/csv-chartjs/src/features/colors.ts
+++ chartJS/csv-chartjs/src/features/colors.ts
@@ -1,17 +1,17 @@
- import randomColor from 'node-random-color'
+//  import randomColor from 'node-random-color'
  export const getColors =  async (data:any) => {
     
     let backGroundColor:any = []
     for(let i = 0; i < data.length; i++) {
 
-      const color = randomColor({
-          difference: data.length,
-          considerations: data.length,
-          brightness:true
-      });
-      backGroundColor.push(color)
+      // const color = randomColor({
+      //     difference: data.length,
+      //     considerations: data.length,
+      //     brightness:true
+      // });
+      // backGroundColor.push(color)
   
-      console.log(color);
+      // console.log(color);
       
   }
   return backGroundColor
